582ff8c718f33c500baf2fcbadf2e53b2dc743dd
Parth Vadodaria (Cloud + Platform Eng) <parth.vadodaria@intuitive.ai>
2026-02-17 21:10:08 +0530

    feat: Implemented navbar UI containing logo and search-bar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
 import './App.css'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
-      <h1 className='text-5xl text-red-500 text-center m-4 font-bold underline'>
-        Start your Frontend Development.
-      </h1>
-    </>
+    <Navbar />
   )
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <nav className="p-2 flex gap-2">
+            <div className="self-center font-[cursive] text-xl">Weather</div>
+            <div className="w-full">
+                <div className="w-fit md:w-7/10 m-auto rounded-md shadow-xl/20 ">
+                    <div className="md:w-full px-2 py-1 flex gap-2">
+                        <i className="ri-send-plane-fill text-violet-700"/>
+                        <input type="search" placeholder="Location" className="md:w-full focus:outline-0"/>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
