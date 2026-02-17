10f5a088166a6984c89d8440755b5ba2e45f2708
Parth Vadodaria (Cloud + Platform Eng) <parth.vadodaria@intuitive.ai>
2026-02-18 01:12:13 +0530

    test: testing the branch protection rule working.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className="text-5xl text-red-500 text-center m-4 font-bold underline">
+      <h1 className="text-5xl text-red-500 text-center m-4 font-italic underline">
         Start your Frontend Development.
       </h1>
     </>
