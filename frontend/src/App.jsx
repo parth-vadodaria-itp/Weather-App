97be58723199e0038d27c3ec7f8c4fbccf8d4eea
Parth Vadodaria (Cloud + Platform Eng) <parth.vadodaria@intuitive.ai>
2026-02-18 00:59:00 +0530

    build: Implemented formatting by integrating prettier and linting using ESLint.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,11 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className='text-5xl text-red-500 text-center m-4 font-bold underline'>
+      <h1 className="text-5xl text-red-500 text-center m-4 font-bold underline">
         Start your Frontend Development.
       </h1>
     </>
   )
 }
 
-export default App;
+export default App
