3072ca5da4196ef568b58fef27a8f796bc629458
parth-vadodaria-itp <Parth.Vadodaria@intuitive.ai>
2026-02-20 13:16:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 
 function App() {
+  const fig="testing branch protection rule";
   return (
     <>
       <h1 className="text-5xl text-red-500 text-center m-4 font-italic underline">
